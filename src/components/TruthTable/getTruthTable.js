a077f1970eb654bbe0d438784c4958a6781d9bc9
Daniel <daniel-lutz@live.at>
2018-11-01 18:01:28 +0100

    getTruthTable: add comments to clarify behaviour

--- src/components/TruthTable/getTruthTable.js
+++ src/components/TruthTable/getTruthTable.js
@@ -1,13 +1,17 @@
 export const getHeaders = numberOfInputs => {
   const headers = []
+
+  // Fill headers array with increasing numbers starting from 0
   for (let i = 0; i < numberOfInputs; i++) {
     headers.push(i)
   }
+
   return headers
 }
 
 export const getRows = numberOfInputs => {
   const rows = []
+
   // Create 2^n rows
   for (let rowNumber = 0; rowNumber < 2 ** numberOfInputs; rowNumber++) {
     rows.push(getRow(rowNumber, numberOfInputs))
@@ -19,14 +23,17 @@ export const getRows = numberOfInputs => {
 const getRow = (rowNumber, numberOfInputs) => {
   const row = []
 
+  // Fill all rows with an increasing binary number whose digits are split over row cells
   for (let cellNumber = 0; cellNumber < numberOfInputs; cellNumber++) {
     row.push(getCellValue(rowNumber, cellNumber))
   }
+
   // Reverse the array to get higher bits at the front
   return row.reverse()
 }
 
-const getCellValue = (rowNumber, cellNumber) => {
-  // Right shift the binary value of the rowNumber by cellNumber bits
-  return (rowNumber >> cellNumber) & 1
+const getCellValue = (rowNumber, n) => {
+  // Right shift the binary value of the rowNumber by n bits
+  // Binary AND comparison results in 1 or 0 values
+  return (rowNumber >> n) & 1
 }
