ebc69d5a81ef40a36e5d3959b3db1a41c4d6ccbb
Daniel <daniel-lutz@live.at>
2018-11-04 17:38:42 +0100

    Spell lower case to suppress react warning

--- src/components/TruthTable/TruthTable.js
+++ src/components/TruthTable/TruthTable.js
@@ -8,14 +8,14 @@ const ResultToggleButton = styled(Button)`
   padding: 0.25rem 0.75rem;
   margin: 0;
   font-size: 0.85rem;
-  background-color: ${({ isTrue }) => (isTrue ? '#01ff70' : '#6c757d')};
-  border-color: ${({ isTrue }) => (isTrue ? '#01ff70' : '#6c757d')};
-  color: ${({ isTrue }) => (isTrue ? '#000' : '#fff')};
+  background-color: ${({ istrue }) => (istrue ? '#01ff70' : '#6c757d')};
+  border-color: ${({ istrue }) => (istrue ? '#01ff70' : '#6c757d')};
+  color: ${({ istrue }) => (istrue ? '#000' : '#fff')};
 
   &:hover {
-    background-color: ${({ isTrue }) => (isTrue ? '#01ff70' : '#6c757d')};
-    border-color: ${({ isTrue }) => (isTrue ? '#01ff70' : '#6c757d')};
-    color: ${({ isTrue }) => (isTrue ? '#000' : '#fff')};
+    background-color: ${({ istrue }) => (istrue ? '#01ff70' : '#6c757d')};
+    border-color: ${({ istrue }) => (istrue ? '#01ff70' : '#6c757d')};
+    color: ${({ istrue }) => (istrue ? '#000' : '#fff')};
   }
 `
 
@@ -63,7 +63,7 @@ const TruthTable = ({
                     <td>
                       <ResultToggleButton
                         onClick={() => toggleResult(rowNumber)}
-                        isTrue={result}
+                        istrue={result}
                       >
                         {result ? 'True' : 'False'}
                       </ResultToggleButton>
